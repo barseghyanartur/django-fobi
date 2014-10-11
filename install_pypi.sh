09a935eed83d602c73f76929f381fabd8114a669
Artur Barseghyan <artur.barseghyan@gmail.com>
2014-10-11 21:24:29 +0200

    update helper scripts

--- install_pypi.sh
+++ install_pypi.sh
@@ -1,6 +1,6 @@
 #pip install -r examples/requirements.txt --allow-all-external --allow-unverified django-admin-tools
 pip install -r examples/requirements.txt
-python setup.py install
+pip install django-fobi
 mkdir -p examples/logs examples/db examples/media examples/media/static examples/media/fobi_plugins/content_image
 mkdir -p examples/media/fobi_plugins/file
 python examples/simple/manage.py collectstatic --noinput
