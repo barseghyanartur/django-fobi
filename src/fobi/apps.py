a9c7bde5618066de6bf6c3b00f60b66e15f85158
Artur Barseghyan <artur.barseghyan@gmail.com>
2016-09-15 22:22:11 +0200

    apps.py pep8

--- src/fobi/apps.py
+++ src/fobi/apps.py
@@ -1,6 +1,6 @@
 __title__ = 'fobi.apps'
 __author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = 'Copyright (c) 2014-2015 Artur Barseghyan'
+__copyright__ = '2014-2016 Artur Barseghyan'
 __license__ = 'GPL 2.0/LGPL 2.1'
 __all__ = ('Config',)
 
@@ -8,6 +8,8 @@ try:
     from django.apps import AppConfig
 
     class Config(AppConfig):
+        """Config."""
+
         name = label = 'fobi'
 
 except ImportError:
