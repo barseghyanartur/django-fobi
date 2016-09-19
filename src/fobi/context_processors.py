3a5eb6a637af5da6caccd7205b1508e715064624
Artur Barseghyan <barseghyan@goldmund-wyldebeast-wunderliebe.com>
2016-09-19 16:11:18 +0200

    context_processors.py pep8

--- src/fobi/context_processors.py
+++ src/fobi/context_processors.py
@@ -1,28 +1,27 @@
-__title__ = 'fobi.context_processors'
-__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = 'Copyright (c) 2014 Artur Barseghyan'
-__license__ = 'GPL 2.0/LGPL 2.1'
-__all__ = ('theme', 'dynamic_values', 'form_importers',)
-
 import datetime
 
 from fobi.base import get_theme
-from fobi.helpers import StrippedRequest
 from fobi.form_importers import get_form_impoter_plugin_urls
+from fobi.helpers import StrippedRequest
+
+__title__ = 'fobi.context_processors'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('dynamic_values', 'form_importers', 'theme',)
+
 
 def theme(request):
-    """
-    Gets active theme.
+    """Get active theme.
 
     :param django.http.HttpRequest request:
     :return fobi.base.BaseTheme: Instance of ``fobi.base.BaseTheme``.
     """
     return {'fobi_theme': get_theme(request, as_instance=True)}
 
+
 def dynamic_values(request):
-    """
-    Dynamic values exposed to public forms.
-    """
+    """Dynamic values exposed to public forms."""
     return {
         'fobi_dynamic_values': {
             'request': StrippedRequest(request),
@@ -31,10 +30,9 @@ def dynamic_values(request):
         }
     }
 
+
 def form_importers(request):
-    """
-    Form importers.
-    """
+    """Form importers."""
     return {
         'form_importers': get_form_impoter_plugin_urls(),
     }
