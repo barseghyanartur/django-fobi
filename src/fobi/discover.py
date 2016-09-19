c75a7d53abdc361b04b895d0c1d3d7b224c53a5b
Artur Barseghyan <barseghyan@goldmund-wyldebeast-wunderliebe.com>
2016-09-19 16:23:42 +0200

    discover.py pep8

--- src/fobi/discover.py
+++ src/fobi/discover.py
@@ -1,9 +1,3 @@
-__title__ = 'fobi.discover'
-__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = 'Copyright (c) 2014-2015 Artur Barseghyan'
-__license__ = 'GPL 2.0/LGPL 2.1'
-__all__ = ('autodiscover',)
-
 import imp
 import logging
 import sys
@@ -14,21 +8,26 @@ from django.conf import settings
 
 from nine.versions import DJANGO_GTE_1_7
 
+from .conf import get_setting
+
 logger = logging.getLogger(__file__)
 
 # In Django a dotted path can be used up to the app config class. In
 # such cases the old-school autodiscovery of modules doesn't work but we
-# have a great Django `autodiscover_modules` tool then. In cases if Django 
+# have a great Django `autodiscover_modules` tool then. In cases if Django
 # version is >= 1.7, we use the Django `autodiscover_modules` tool falling back
 # to our own implementation of it otherwise.
 if DJANGO_GTE_1_7:
     from django.utils.module_loading import autodiscover_modules
 else:
     def autodiscover_modules(module_name):
+        """Auto-discover modules."""
         for app in settings.INSTALLED_APPS:
             try:
                 app = str(app)
-                app_path = __import__(app, {}, {}, [app.split('.')[-1]]).__path__
+                app_path = __import__(
+                    app, {}, {}, [app.split('.')[-1]]
+                ).__path__
             except (AttributeError, TypeError) as e:
                 logger.debug(str(e))
                 continue
@@ -40,12 +39,16 @@ else:
                 continue
             __import__('{0}.{1}'.format(app, module_name))
 
-from fobi.conf import get_setting
+
+__title__ = 'fobi.discover'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = 'Copyright (c) 2014-2015 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('autodiscover',)
+
 
 def autodiscover():
-    """
-    Auto-discovers files that should be found by fobi.
-    """
+    """Auto-discovers files that should be found by fobi."""
     # For Python 3 we need to increase the recursion limit, otherwise things
     # break. What we want is to set the recursion limit back to its' initial
     # value after all plugins have been discovered.
