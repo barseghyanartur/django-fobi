7678cba65dc5b2b9af87f71a8d438fcd28c76a36
Artur Barseghyan <artur.barseghyan@gmail.com>
2016-09-19 22:25:10 +0200

    test.py pep8

--- src/fobi/test.py
+++ src/fobi/test.py
@@ -1,16 +1,17 @@
 from __future__ import print_function
 
-__title__ = 'fobi.test'
-__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = 'Copyright (c) 2014 Artur Barseghyan'
-__license__ = 'GPL 2.0/LGPL 2.1'
-
 import unittest
 
-from fobi.tests.test_browser_build_dynamic_forms import *
-from fobi.tests.test_core import *
-from fobi.tests.test_dynamic_forms import *
-from fobi.tests.test_sortable_dict import *
+from .tests.test_browser_build_dynamic_forms import *
+from .tests.test_core import *
+from .tests.test_dynamic_forms import *
+from .tests.test_sortable_dict import *
+
+__title__ = 'fobi.test'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+
 
 if __name__ == '__main__':
     unittest.main()
