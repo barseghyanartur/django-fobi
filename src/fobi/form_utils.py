b92b8484cd5549098c7ecf1202e7c4af167d14f9
Artur Barseghyan <barseghyan@goldmund-wyldebeast-wunderliebe.com>
2016-09-19 16:36:03 +0200

    form_utils.py pep8

--- src/fobi/form_utils.py
+++ src/fobi/form_utils.py
@@ -1,12 +1,14 @@
+from django.utils.encoding import force_text
+
+from nine.versions import DJANGO_GTE_1_7
+
 __title__ = 'fobi.form_utils'
 __author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = 'Copyright (c) 2014 Artur Barseghyan'
+__copyright__ = '2014-2016 Artur Barseghyan'
 __license__ = 'GPL 2.0/LGPL 2.1'
 __all__ = (
     'ErrorDict', 'ErrorList',
 )
-
-from nine.versions import DJANGO_GTE_1_7
 
 if DJANGO_GTE_1_7:
     from django.forms.utils import (
@@ -17,24 +19,23 @@ else:
         ErrorDict as DjangoErrorDict, ErrorList as DjangoErrorList
     )
 
-from django.utils.encoding import force_text
-
 
 class ErrorDict(DjangoErrorDict):
-    """
-    A better ErrorDict.
-    """
+    """A better ErrorDict."""
+
     def as_text(self):
+        """As text."""
         return '\n'.join(
-            [' %s\n%s' % (k, '\n'.join([' %s' % force_text(i) for i in v])) \
+            [' %s\n%s' % (k, '\n'.join([' %s' % force_text(i) for i in v]))
              for k, v in self.items()]
-            )
+        )
 
 
 class ErrorList(DjangoErrorList):
-    """
-    A better ErrorList.
-    """
+    """A better ErrorList."""
+
     def as_text(self):
-        if not self: return ''
+        """As text."""
+        if not self:
+            return ''
         return '\n'.join([' %s' % force_text(e) for e in self])
