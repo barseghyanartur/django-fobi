39d8340b9208956e892eeababf7128ce1c5097c0
Artur Barseghyan <barseghyan@goldmund-wyldebeast-wunderliebe.com>
2016-09-19 15:55:18 +0200

    compat.py pep8

--- src/fobi/compat.py
+++ src/fobi/compat.py
@@ -1,22 +1,11 @@
-__title__ = 'fobi.compat'
-__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = 'Copyright (c) 2013-2015 Artur Barseghyan'
-__license__ = 'GPL 2.0/LGPL 2.1'
-__all__ = ('AUTH_USER_MODEL', 'User',)
-
 from django.conf import settings
 
 from nine.user import User
 
-# Sanity checks. Possibly rely on the dynamic username field in future.
-#user = User()
-#
-#if not hasattr(user, 'username'):
-#    from fobi.exceptions import ImproperlyConfigured
-#    raise ImproperlyConfigured("Your custom user model ({0}.{1}) doesn't "
-#                               "have ``username`` property, while "
-#                               "``django-fobi`` relies on its' presence."
-#                               "".format(user._meta.app_label,
-#                                         user._meta.object_name))
+__title__ = 'fobi.compat'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2013-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('AUTH_USER_MODEL', 'User',)
 
 AUTH_USER_MODEL = settings.AUTH_USER_MODEL
