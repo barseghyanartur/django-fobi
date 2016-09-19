661ff7c575189d7bca661ff7f396f2bcaf9bafdb
Artur Barseghyan <barseghyan@goldmund-wyldebeast-wunderliebe.com>
2016-09-19 16:29:07 +0200

    exceptions.py pep8

--- src/fobi/exceptions.py
+++ src/fobi/exceptions.py
@@ -1,84 +1,70 @@
 __title__ = 'fobi.exceptions'
 __author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = '2014-2015 Artur Barseghyan'
+__copyright__ = '2014-2016 Artur Barseghyan'
 __license__ = 'GPL 2.0/LGPL 2.1'
 __all__ = (
-    'BaseException', 'ImproperlyConfigured', 'InvalidRegistryItemType', 
+    'BaseException', 'ImproperlyConfigured', 'InvalidRegistryItemType',
     'DoesNotExist', 'ThemeDoesNotExist', 'PluginDoesNotExist',
     'FormElementPluginDoesNotExist', 'FormHandlerPluginDoesNotExist',
     'NoDefaultThemeSet', 'FormPluginError', 'FormElementPluginError',
     'FormHandlerPluginError', 'FormCallbackError',
 )
 
+
 class BaseException(Exception):
-    """
-    Base exception.
-    """
+    """Base exception."""
 
 
 class ImproperlyConfigured(BaseException):
-    """
+    """Improperly configured.
+
     Exception raised when developer didn't configure/write the code properly.
     """
 
 
 class InvalidRegistryItemType(ValueError, BaseException):
-    """
+    """Invalid registry item type.
+
     Raised when an attempt is made to register an item in the registry which
     does not have a proper type.
     """
 
 
 class DoesNotExist(BaseException):
-    """
-    Raised when something does not exist.
-    """
+    """Raised when something does not exist."""
 
 
 class ThemeDoesNotExist(DoesNotExist):
-    """
-    Raised when no theme with given uid can be found.
-    """
+    """Raised when no theme with given uid can be found."""
 
 
 class PluginDoesNotExist(DoesNotExist):
-    """
-    Raised when no plugin with given uid can be found.
-    """
+    """Raised when no plugin with given uid can be found."""
 
 
 class FormElementPluginDoesNotExist(PluginDoesNotExist):
-    """
-    Raised when no form element plugin with given uid can be found.
-    """
+    """Raised when no form element plugin with given uid can be found."""
 
 
 class FormHandlerPluginDoesNotExist(PluginDoesNotExist):
-    """
-    Raised when no form handler plugin with given uid can be found.
-    """
+    """Raised when no form handler plugin with given uid can be found."""
+
 
 class NoDefaultThemeSet(ImproperlyConfigured):
-    """
-    Raised when no active theme is chosen.
-    """
+    """Raised when no active theme is chosen."""
+
 
 class FormPluginError(BaseException):
-    """
-    Base error for form elements and handers.
-    """
+    """Base error for form elements and handlers."""
+
 
 class FormElementPluginError(FormPluginError):
-    """
-    Raised when form element plugin error occurs.
-    """
+    """Raised when form element plugin error occurs."""
+
 
 class FormHandlerPluginError(FormPluginError):
-    """
-    Raised when form handler plugin error occurs.
-    """
+    """Raised when form handler plugin error occurs."""
+
 
 class FormCallbackError(FormPluginError):
-    """
-    Raised when form callback error occurs.
-    """
+    """Raised when form callback error occurs."""
