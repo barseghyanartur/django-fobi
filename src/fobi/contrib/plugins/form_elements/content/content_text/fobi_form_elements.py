47d050f74cc81745b9d364a2550de145d9fc6f55
Artur Barseghyan <barseghyan@goldmund-wyldebeast-wunderliebe.com>
2016-09-20 11:52:49 +0200

    content_text pep8

--- src/fobi/contrib/plugins/form_elements/content/content_text/fobi_form_elements.py
+++ src/fobi/contrib/plugins/form_elements/content/content_text/fobi_form_elements.py
@@ -1,13 +1,7 @@
-__title__ = 'fobi.contrib.plugins.form_elements.content.content_text.fobi_form_elements'
-__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = 'Copyright (c) 2014-2015 Artur Barseghyan'
-__license__ = 'GPL 2.0/LGPL 2.1'
-__all__ = ('ContentTextPlugin',)
-
 from uuid import uuid4
 
-from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import smart_str
+from django.utils.translation import ugettext_lazy as _
 
 from nonefield.fields import NoneField
 
@@ -16,25 +10,31 @@ from fobi.base import FormElementPlugin, form_element_plugin_registry
 from . import UID
 from .forms import ContentTextForm
 
+__title__ = 'fobi.contrib.plugins.form_elements.content.content_text.' \
+            'fobi_form_elements'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('ContentTextPlugin',)
+
+
 class ContentTextPlugin(FormElementPlugin):
-    """
-    Content text plugin.
-    """
+    """Content text plugin."""
+
     uid = UID
     name = _("Content text")
     group = _("Content")
     form = ContentTextForm
 
     def post_processor(self):
-        """
+        """Post process data.
+
         Always the same.
         """
         self.data.name = "{0}_{1}".format(self.uid, uuid4())
 
     def get_form_field_instances(self, request=None):
-        """
-        Get form field instances.
-        """
+        """Get form field instances."""
         kwargs = {
             'initial': "<p>{0}</p>".format(smart_str(self.data.text)),
             'required': False,
