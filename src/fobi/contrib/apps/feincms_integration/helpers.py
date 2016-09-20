4d83194d7fc2a9222c36bf6d9877783389b3429d
Artur Barseghyan <barseghyan@goldmund-wyldebeast-wunderliebe.com>
2016-09-20 11:20:03 +0200

    feincms_integration pep8

--- src/fobi/contrib/apps/feincms_integration/helpers.py
+++ src/fobi/contrib/apps/feincms_integration/helpers.py
@@ -1,17 +1,18 @@
-__title__ = 'fobi.contrib.apps.feincms_integration.helpers'
-__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = 'Copyright (c) 2014-2015 Artur Barseghyan'
-__license__ = 'GPL 2.0/LGPL 2.1'
-__all__ = ('get_form_template_choices', 'get_success_page_template_choices',)
-
 from fobi.integration.helpers import get_template_choices
 
 from .settings import FORM_TEMPLATE_CHOICES, SUCCESS_PAGE_TEMPLATE_CHOICES
 
+__title__ = 'fobi.contrib.apps.feincms_integration.helpers'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('get_form_template_choices', 'get_success_page_template_choices',)
+
+
 def get_form_template_choices():
-    """
-    Gets the form template choices. It's possible to provide theme templates
-    per theme or just per project.
+    """Gets the form template choices.
+
+    It's possible to provide theme templates per theme or just per project.
 
     :return list:
     """
@@ -19,14 +20,16 @@ def get_form_template_choices():
         'feincms_integration',
         FORM_TEMPLATE_CHOICES,
         'form_template_choices'
-        )
+    )
+
 
 def get_success_page_template_choices():
-    """
+    """Get success page template choices.
+
     :return list:
     """
     return get_template_choices(
         'feincms_integration',
         SUCCESS_PAGE_TEMPLATE_CHOICES,
         'success_page_template_choices'
-        )
+    )
--- src/fobi/contrib/apps/feincms_integration/widgets.py
+++ src/fobi/contrib/apps/feincms_integration/widgets.py
@@ -1,26 +1,27 @@
-__title__ = 'fobi.contrib.apps.feincms_integration.widgets'
-__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = 'Copyright (c) 2014-2015 Artur Barseghyan'
-__license__ = 'GPL 2.0/LGPL 2.1'
-__all__ = ('FobiFormWidget',)
-
 from django.db import models
 from django.utils.translation import ugettext, ugettext_lazy as _
 
 from fobi.integration.processors import IntegrationProcessor
 
-from .settings import WIDGET_FORM_SENT_GET_PARAM
 from .helpers import (
     get_form_template_choices, get_success_page_template_choices
-    )
+)
+from .settings import WIDGET_FORM_SENT_GET_PARAM
+
+__title__ = 'fobi.contrib.apps.feincms_integration.widgets'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('FobiFormWidget',)
+
 
 class FobiFormWidget(models.Model, IntegrationProcessor):
-    """
-    Widget for to FeinCMS.
+    """Widget for to FeinCMS.
 
     :property fobi.models.FormEntry form_entry: Form entry to be rendered.
     :property str template: If given used for rendering the form.
     """
+
     # Fobi integration processor configuration
     form_sent_get_param = WIDGET_FORM_SENT_GET_PARAM
     can_redirect = True
@@ -32,55 +33,55 @@ class FobiFormWidget(models.Model, IntegrationProcessor):
         _("Form template name"), max_length=255, null=True, blank=True,
         choices=get_form_template_choices(),
         help_text=_("Template to render the form with.")
-        )
+    )
 
     hide_form_title = models.BooleanField(
         _("Hide form title"), default=False,
         help_text=_("If checked, no form title is shown.")
-        )
+    )
 
     form_title = models.CharField(
         _("Form title"), max_length=255, null=True, blank=True,
         help_text=_("Overrides the default form title.")
-        )
+    )
 
     form_submit_button_text = models.CharField(
         _("Submit button text"), max_length=255, null=True, blank=True,
         help_text=_("Overrides the default form submit button text.")
-        )
+    )
 
     success_page_template_name = models.CharField(
         _("Success page template name"), max_length=255, null=True, blank=True,
         choices=get_success_page_template_choices(),
         help_text=_("Template to render the success page with.")
-        )
+    )
 
     hide_success_page_title = models.BooleanField(
         _("Hide success page title"), default=False,
         help_text=_("If checked, no success page title is shown.")
-        )
+    )
 
     success_page_title = models.CharField(
         _("Succes page title"), max_length=255, null=True, blank=True,
         help_text=_("Overrides the default success page title.")
-        )
+    )
 
     success_page_text = models.TextField(
         _("Succes page text"), null=True, blank=True,
         help_text=_("Overrides the default success page text.")
-        )
+    )
 
     class Meta:
+        """Meta class."""
         abstract = True
         app_label = 'fobi'
-        #db_table = 'page_page_fobiformwidget'
+        # db_table = 'page_page_fobiformwidget'
 
     def __unicode__(self):
         return ugettext('Fobi form')
 
     def process(self, request, **kwargs):
-        """
-        This is where most of the form handling happens.
+        """This is where most of the form handling happens.
 
         :param django.http.HttpRequest request:
         :return django.http.HttpResponse | str:
@@ -88,8 +89,10 @@ class FobiFormWidget(models.Model, IntegrationProcessor):
         return self._process(request, self, **kwargs)
 
     def render(self, **kwargs):
+        """Render."""
         return getattr(self, 'rendered_output', '')
 
     def finalize(self, request, response):
+        """Finalize."""
         # Always disable caches if this content type is used somewhere
         response['Cache-Control'] = 'no-cache, must-revalidate'
