bde4372b759047defe1303905089b43e2f35b104
Artur Barseghyan <barseghyan@goldmund-wyldebeast-wunderliebe.com>
2016-09-20 17:01:53 +0200

    url pep8

--- src/fobi/contrib/plugins/form_elements/fields/url/fobi_form_elements.py
+++ src/fobi/contrib/plugins/form_elements/fields/url/fobi_form_elements.py
@@ -1,16 +1,13 @@
-__title__ = 'fobi.contrib.plugins.form_elements.fields.url.fobi_form_elements'
-__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = '2014-2016 Artur Barseghyan'
-__license__ = 'GPL 2.0/LGPL 2.1'
-__all__ = ('URLInputPlugin',)
-
 from django.forms.fields import URLField
 
 try:
     from django.forms.widgets import URLInput
 except ImportError:
     from django.forms.widgets import TextInput
+
     class URLInput(TextInput):
+        """URL input."""
+
         input_type = 'url'
 
 from django.utils.translation import ugettext_lazy as _
@@ -20,21 +17,25 @@ from fobi.base import FormFieldPlugin, form_element_plugin_registry, get_theme
 from . import UID
 from .forms import URLInputForm
 
+__title__ = 'fobi.contrib.plugins.form_elements.fields.url.fobi_form_elements'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('URLInputPlugin',)
+
 theme = get_theme(request=None, as_instance=True)
 
+
 class URLInputPlugin(FormFieldPlugin):
-    """
-    URL input plugin.
-    """
+    """URL input plugin."""
+
     uid = UID
     name = _("URL")
     group = _("Fields")
     form = URLInputForm
 
     def get_form_field_instances(self, request=None):
-        """
-        Get form field instances.
-        """
+        """Get form field instances."""
         widget_attrs = {
             'class': theme.form_element_html_class,
             'type': 'url',
