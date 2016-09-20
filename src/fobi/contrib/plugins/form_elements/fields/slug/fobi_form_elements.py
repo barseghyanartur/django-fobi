e5b685417209dd36cb30d82c93946fa189e3756c
Artur Barseghyan <barseghyan@goldmund-wyldebeast-wunderliebe.com>
2016-09-20 16:50:50 +0200

    slug pep8

--- src/fobi/contrib/plugins/form_elements/fields/slug/fobi_form_elements.py
+++ src/fobi/contrib/plugins/form_elements/fields/slug/fobi_form_elements.py
@@ -1,9 +1,3 @@
-__title__ = 'fobi.contrib.plugins.form_elements.fields.slug.fobi_form_elements'
-__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = '2014-2016 Artur Barseghyan'
-__license__ = 'GPL 2.0/LGPL 2.1'
-__all__ = ('SlugInputPlugin',)
-
 from django.forms.fields import SlugField
 from django.forms.widgets import TextInput
 from django.utils.translation import ugettext_lazy as _
@@ -13,21 +7,25 @@ from fobi.base import FormFieldPlugin, form_element_plugin_registry, get_theme
 from . import UID
 from .forms import SlugInputForm
 
+__title__ = 'fobi.contrib.plugins.form_elements.fields.slug.fobi_form_elements'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('SlugInputPlugin',)
+
 theme = get_theme(request=None, as_instance=True)
 
+
 class SlugInputPlugin(FormFieldPlugin):
-    """
-    Slug field plugin.
-    """
+    """Slug field plugin."""
+
     uid = UID
     name = _("Slug")
     group = _("Fields")
     form = SlugInputForm
 
     def get_form_field_instances(self, request=None):
-        """
-        Get form field instances.
-        """
+        """Get form field instances."""
         widget_attrs = {
             'class': theme.form_element_html_class,
             'placeholder': self.data.placeholder,
