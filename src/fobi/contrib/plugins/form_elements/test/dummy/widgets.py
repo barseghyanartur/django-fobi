e98d260e745941d94ea91c2684edb4adff09cfde
Artur Barseghyan <barseghyan@goldmund-wyldebeast-wunderliebe.com>
2016-09-20 17:20:48 +0200

    dummy pep8

--- src/fobi/contrib/plugins/form_elements/test/dummy/widgets.py
+++ src/fobi/contrib/plugins/form_elements/test/dummy/widgets.py
@@ -1,15 +1,15 @@
+from fobi.base import FormElementPluginWidget
+
+from . import UID
+
 __title__ = 'fobi.contrib.plugins.form_elements.test.dummy.widgets'
 __author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
 __copyright__ = '2014-2016 Artur Barseghyan'
 __license__ = 'GPL 2.0/LGPL 2.1'
 __all__ = ('BaseDummyPluginWidget',)
 
-from fobi.base import FormElementPluginWidget
-
-from . import UID
 
 class BaseDummyPluginWidget(FormElementPluginWidget):
-    """
-    Base dummy form element plugin widget.
-    """
+    """Base dummy form element plugin widget."""
+
     plugin_uid = UID
