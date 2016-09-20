d5e974e447b377f6a55d70867f3bd841178f428b
Artur Barseghyan <barseghyan@goldmund-wyldebeast-wunderliebe.com>
2016-09-20 16:55:47 +0200

    textarea pep8

--- src/fobi/contrib/plugins/form_elements/fields/textarea/fobi_form_elements.py
+++ src/fobi/contrib/plugins/form_elements/fields/textarea/fobi_form_elements.py
@@ -1,9 +1,3 @@
-__title__ = 'fobi.contrib.plugins.form_elements.fields.textarea.fobi_form_elements'
-__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = '2014-2016 Artur Barseghyan'
-__license__ = 'GPL 2.0/LGPL 2.1'
-__all__ = ('TextInputPlugin',)
-
 from django.forms.fields import CharField
 from django.forms.widgets import Textarea
 from django.utils.translation import ugettext_lazy as _
@@ -13,21 +7,26 @@ from fobi.base import FormFieldPlugin, form_element_plugin_registry, get_theme
 from . import UID
 from .forms import TextareaForm
 
+__title__ = 'fobi.contrib.plugins.form_elements.fields.' \
+            'textarea.fobi_form_elements'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('TextInputPlugin',)
+
 theme = get_theme(request=None, as_instance=True)
 
+
 class TextareaPlugin(FormFieldPlugin):
-    """
-    Char field plugin.
-    """
+    """Textarea field plugin."""
+
     uid = UID
     name = _("Textarea")
     group = _("Fields")
     form = TextareaForm
 
     def get_form_field_instances(self, request=None):
-        """
-        Get form field instances.
-        """
+        """Get form field instances."""
         widget_attrs = {
             'class': theme.form_element_html_class,
             'placeholder': self.data.placeholder,
