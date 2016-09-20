eda92332fbe5b8acf3a3971244410d943f076e06
Artur Barseghyan <barseghyan@goldmund-wyldebeast-wunderliebe.com>
2016-09-20 15:17:23 +0200

    regex pep8

--- src/fobi/contrib/plugins/form_elements/fields/regex/fobi_form_elements.py
+++ src/fobi/contrib/plugins/form_elements/fields/regex/fobi_form_elements.py
@@ -1,11 +1,5 @@
 from __future__ import absolute_import
 
-__title__ = 'fobi.contrib.plugins.form_elements.fields.regex.fobi_form_elements'
-__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = '2014-2016 Artur Barseghyan'
-__license__ = 'GPL 2.0/LGPL 2.1'
-__all__ = ('RegexInputPlugin',)
-
 from django.forms.fields import RegexField
 from django.forms.widgets import TextInput
 from django.utils.translation import ugettext_lazy as _
@@ -15,21 +9,26 @@ from fobi.base import FormFieldPlugin, form_element_plugin_registry, get_theme
 from . import UID
 from .forms import RegexInputForm
 
+__title__ = 'fobi.contrib.plugins.form_elements.fields.' \
+            'regex.fobi_form_elements'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('RegexInputPlugin',)
+
 theme = get_theme(request=None, as_instance=True)
 
+
 class RegexInputPlugin(FormFieldPlugin):
-    """
-    Regex field plugin.
-    """
+    """Regex field plugin."""
+
     uid = UID
     name = _("Regex")
     group = _("Fields")
     form = RegexInputForm
 
     def get_form_field_instances(self, request=None):
-        """
-        Get form field instances.
-        """
+        """Get form field instances."""
         widget_attrs = {
             'class': theme.form_element_html_class,
             'placeholder': self.data.placeholder,
--- src/fobi/contrib/plugins/form_elements/fields/regex/forms.py
+++ src/fobi/contrib/plugins/form_elements/fields/regex/forms.py
@@ -1,23 +1,23 @@
 from __future__ import absolute_import
 
-__title__ = 'fobi.contrib.plugins.form_elements.fields.regex.forms'
-__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = '2014-2016 Artur Barseghyan'
-__license__ = 'GPL 2.0/LGPL 2.1'
-__all__ = ('RegexInputForm',)
-
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 from fobi.base import BaseFormFieldPluginForm, get_theme
 from fobi.settings import DEFAULT_MAX_LENGTH
 
+__title__ = 'fobi.contrib.plugins.form_elements.fields.regex.forms'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('RegexInputForm',)
+
 theme = get_theme(request=None, as_instance=True)
 
+
 class RegexInputForm(forms.Form, BaseFormFieldPluginForm):
-    """
-    Form for ``RegexInputPlugin``.
-    """
+    """Form for ``RegexInputPlugin``."""
+
     plugin_data_fields = [
         ("label", ""),
         ("name", ""),
@@ -32,67 +32,67 @@ class RegexInputForm(forms.Form, BaseFormFieldPluginForm):
     label = forms.CharField(
         label=_("Label"),
         required=True,
-        widget = forms.widgets.TextInput(
+        widget=forms.widgets.TextInput(
             attrs={'class': theme.form_element_html_class}
         )
     )
     name = forms.CharField(
         label=_("Name"),
         required=True,
-        widget = forms.widgets.TextInput(
+        widget=forms.widgets.TextInput(
             attrs={'class': theme.form_element_html_class}
         )
     )
     help_text = forms.CharField(
         label=_("Help text"),
         required=False,
-        widget = forms.widgets.Textarea(
+        widget=forms.widgets.Textarea(
             attrs={'class': theme.form_element_html_class}
         )
     )
     regex = forms.RegexField(
-        label = _("Regex"),
+        label=_("Regex"),
         required=True,
-        widget = forms.widgets.TextInput(
+        widget=forms.widgets.TextInput(
             attrs={'class': theme.form_element_html_class}
         ),
-        regex = "",
-        help_text = _("Enter a valid regular expression. A couple of common "
-                      "examples are listed below.<br/>"
-                      "- Allow a single digit from 1 to 9 (example value 6): "
-                      "<code>^[1-9]$</code><br/>"
-                      "- Allow any combination of characters from a to z, "
-                      "including capitals (example value abcXYZ):"
-                      "<code>^([a-zA-Z])+$</code><br/>"
-                      "- Allow a hex value (example value #a5c125:"
-                      "<code>^#?([a-f0-9]{6}|[a-f0-9]{3})$</code><br/>")
+        regex="",
+        help_text=_("Enter a valid regular expression. A couple of common "
+                    "examples are listed below.<br/>"
+                    "- Allow a single digit from 1 to 9 (example value 6): "
+                    "<code>^[1-9]$</code><br/>"
+                    "- Allow any combination of characters from a to z, "
+                    "including capitals (example value abcXYZ):"
+                    "<code>^([a-zA-Z])+$</code><br/>"
+                    "- Allow a hex value (example value #a5c125:"
+                    "<code>^#?([a-f0-9]{6}|[a-f0-9]{3})$</code><br/>")
     )
     initial = forms.CharField(
         label=_("Initial"),
         required=False,
-        widget = forms.widgets.TextInput(
+        widget=forms.widgets.TextInput(
             attrs={'class': theme.form_element_html_class}
         )
     )
     max_length = forms.IntegerField(
         label=_("Max length"),
         required=True,
-        widget = forms.widgets.TextInput(
+        widget=forms.widgets.TextInput(
             attrs={'class': theme.form_element_html_class}
         ),
-        initial = DEFAULT_MAX_LENGTH
+        initial=DEFAULT_MAX_LENGTH
     )
     required = forms.BooleanField(
         label=_("Required"),
         required=False,
-        widget = forms.widgets.CheckboxInput(
+        widget=forms.widgets.CheckboxInput(
             attrs={'class': theme.form_element_checkbox_html_class}
         )
     )
     placeholder = forms.CharField(
         label=_("Placeholder"),
         required=False,
-        widget = forms.widgets.TextInput(
+        widget=forms.widgets.TextInput(
             attrs={'class': theme.form_element_html_class}
         )
     )
