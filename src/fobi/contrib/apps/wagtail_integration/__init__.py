c10aec159f0a6608b6c4d4ea252396d4b15b4cf1
Andy Babic <ababic@rkh.co.uk>
2016-10-26 11:40:50 +0100

    * Removed wagtail_integration files line from .gitignore to enable files to be added * Added a `wagtail_integration` app

---
+++ src/fobi/contrib/apps/wagtail_integration/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'fobi.contrib.apps.wagtail_integration'
+__author__ = 'Andy Babic <andyjbabic@gmail.com>'
+__copyright__ = '2014-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('default_app_config',)
+
+default_app_config = 'fobi.contrib.apps.wagtail_integration.apps.Config'
