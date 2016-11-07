cb5ce7f5d22d7ec1f8cc4d60b32847e20a0072c9
Artur Barseghyan <artur.barseghyan@gmail.com>
2016-11-07 22:41:52 +0100

    prepare 0.9.14; minor fixes

--- src/fobi/__init__.py
+++ src/fobi/__init__.py
@@ -1,6 +1,6 @@
 __title__ = 'django-fobi'
-__version__ = '0.9.13'
-__build__ = 0x00006e
+__version__ = '0.9.14'
+__build__ = 0x00006f
 __author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
 __copyright__ = '2014-2016 Artur Barseghyan'
 __license__ = 'GPL 2.0/LGPL 2.1'
