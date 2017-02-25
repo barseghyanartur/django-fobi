e63ee75ed959dbc35a6c8b706e76dc370bd2175a
Artur Barseghyan <artur.barseghyan@gmail.com>
2017-02-25 22:10:56 +0100

    update prepare docs script

--- scripts/prepare_docs.sh
+++ scripts/prepare_docs.sh
@@ -1,2 +1,2 @@
-cat README.rst SCREENSHOTS.rst docs/documentation.rst.distrib > docs/index.rst
+cat README.rst docs/screenshots.rst.distrib docs/documentation.rst.distrib > docs/index.rst
 cat QUICK_START.rst > docs/quickstart.rst
