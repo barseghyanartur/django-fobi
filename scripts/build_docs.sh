b592349c37d0c7e78a0ac88bbb8e9f54264f516d
Artur Barseghyan <artur.barseghyan@gmail.com>
2017-02-25 22:09:54 +0100

    moved screenshots to docs

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,6 +1,6 @@
 #./scripts/uninstall.sh
 #./scripts/install.sh
-cat README.rst SCREENSHOTS.rst docs/documentation.rst.distrib > docs/index.rst
+cat README.rst docs/screenshots.rst.distrib docs/documentation.rst.distrib > docs/index.rst
 cat QUICK_START.rst > docs/quickstart.rst
 sphinx-build -n -a -b html docs builddocs
 #sphinx-build -n -a -b pdf docs builddocs
