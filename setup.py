0b879cf17479d6b4d3ddf667c2ffd368e40a95a4
Artur Barseghyan <artur.barseghyan@gmail.com>
2017-05-15 00:38:09 +0200

    prepare 0.11.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from distutils.version import LooseVersion
 from setuptools import setup, find_packages
 
-version = '0.11.4'
+version = '0.11.5'
 
 # ***************************************************************************
 # ************************** Python version *********************************
