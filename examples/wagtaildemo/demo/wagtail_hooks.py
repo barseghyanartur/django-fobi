390dbd3b98bbd70a52acbfc24a3eee8563fbb918
Artur Barseghyan <artur.barseghyan@gmail.com>
2017-06-14 01:13:04 +0200

    remove wagtaildemo

--- examples/wagtaildemo/demo/wagtail_hooks.py
+++
@@ -1,8 +0,0 @@
-from django.utils.html import format_html
-from django.conf import settings
-
-from wagtail.wagtailcore import hooks
-
-@hooks.register('insert_editor_css')
-def editor_css():
-    return format_html('<link rel="stylesheet" href="' + settings.STATIC_URL + 'demo/css/admin-streamfield-styles.css">')
--- examples/wagtaildemo/wagtaildemo/heroku_wsgi.py
+++
@@ -1,6 +0,0 @@
-from whitenoise.django import DjangoWhiteNoise
-
-from .wsgi import application as _application
-
-
-application = DjangoWhiteNoise(_application)
