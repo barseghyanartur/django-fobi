85662b4cf86c110f4ecd4432cf5b4ca917833c0e
Artur Barseghyan <artur.barseghyan@gmail.com>
2017-12-24 00:42:55 +0100

    clean up

--- src/fobi/contrib/plugins/form_elements/content/content_image/apps.py
+++ src/fobi/contrib/plugins/form_elements/content/content_image/apps.py
@@ -1,18 +1,14 @@
+from django.apps import AppConfig
+
 __title__ = 'fobi.contrib.plugins.form_elements.content.content_image.apps'
 __author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
 __copyright__ = '2014-2017 Artur Barseghyan'
 __license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('Config',)
 
-try:
-    __all__ = ('Config',)
 
-    from django.apps import AppConfig
+class Config(AppConfig):
+    """Config."""
 
-    class Config(AppConfig):
-        """Config."""
-
-        name = 'fobi.contrib.plugins.form_elements.content.content_image'
-        label = 'fobi_contrib_plugins_form_elements_content_content_image'
-
-except ImportError:
-    pass
+    name = 'fobi.contrib.plugins.form_elements.content.content_image'
+    label = 'fobi_contrib_plugins_form_elements_content_content_image'
