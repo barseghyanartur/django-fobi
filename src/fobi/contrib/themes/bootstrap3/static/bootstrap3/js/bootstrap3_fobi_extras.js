35eb493c37fb1ceb97246b2dcbd01d5b164bdb05
Frantisek Holop <fholop@ripe.net>
2018-01-05 15:38:05 +0100

    introduce a form id so sorting is not dependent on form class

--- src/fobi/contrib/themes/bootstrap3/static/bootstrap3/js/bootstrap3_fobi_extras.js
+++ src/fobi/contrib/themes/bootstrap3/static/bootstrap3/js/bootstrap3_fobi_extras.js
@@ -70,13 +70,13 @@ $(function() {
 
     formElementPositionElements = $('.form-element-position');
     if (formElementPositionElements.length) {
-        $('.form-horizontal .form-group').css({ 'cursor': 'move' });
+        $('#fobi-form .form-group').css({ 'cursor': 'move' });
 
-        $('.form-horizontal').sortable({
+        $('#fobi-form').sortable({
             axis: 'y',
-            items: ".form-group",
+            items: '.form-group',
             update: function(){
-                $.each($('.form-horizontal .form-group'), function(i){
+                $.each($('#fobi-form .form-group'), function(i){
                     $(this).find('input:regex(name, .*-position)').val(i + 1);
                 });
 
