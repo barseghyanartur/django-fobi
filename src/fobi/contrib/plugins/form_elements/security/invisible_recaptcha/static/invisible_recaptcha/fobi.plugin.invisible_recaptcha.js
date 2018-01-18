1365a42850b1ba0a3beb847f978857f49ad4e406
Artur Barseghyan <artur.barseghyan@gmail.com>
2018-01-18 21:28:53 +0100

    update

--- src/fobi/contrib/plugins/form_elements/security/invisible_recaptcha/static/invisible_recaptcha/fobi.plugin.invisible_recaptcha.js
+++ src/fobi/contrib/plugins/form_elements/security/invisible_recaptcha/static/invisible_recaptcha/fobi.plugin.invisible_recaptcha.js
@@ -6,18 +6,19 @@
         Invisible recaptcha plugin init scripts.
 */
 ;
-// TODO: rename .form-horizontal to #fobi-form
 
 function g_recaptcha_onSubmit(token) {
     console.log("g_recaptcha_onSubmit")
 //    document.getElementById("fobi-form").submit();
 //    document.getElementsByClassName("form-horizontal").submit();
-    $('form.form-horizontal').submit();
+//    $('form.form-horizontal').submit();
+    $('form#fobi-form').submit();
 }
 
 $(document).ready(function() {
     var siteKey = window.InvisibleRecaptchaSiteKey || "";
-    var submitFormButton = $('form.form-horizontal button[type=submit]');
+//    var submitFormButton = $('form.form-horizontal button[type=submit]');
+    var submitFormButton = $('form#fobi-form button[type=submit]');
     submitFormButton.addClass('g-recaptcha');
     submitFormButton.attr('data-sitekey', siteKey);
     submitFormButton.attr('data-callback', 'g_recaptcha_onSubmit');
