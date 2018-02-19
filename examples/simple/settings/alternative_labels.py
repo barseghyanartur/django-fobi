45efa80cf89b1b3565974e0d164688e7facf1f5e
Artur Barseghyan <artur.barseghyan@gmail.com>
2018-02-19 21:01:20 +0100

    more

--- examples/simple/settings/alternative_labels.py
+++ examples/simple/settings/alternative_labels.py
@@ -3,6 +3,5 @@ from .base import *
 
 LOCALE_PATHS = [
     os.path.abspath(os.path.join(BASE_DIR, 'fobi_locale')),
-    # os.path.abspath(os.path.join(BASE_DIR, 'locale')),
+    os.path.abspath(os.path.join(BASE_DIR, 'locale')),
 ]
-# import ipdb; ipdb.set_trace()
