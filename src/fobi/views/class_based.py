bb98c202ec007cff8393c76d2212b04d6eddc77c
Kyle Roux <jstacoder@gmail.com>
2018-05-15 14:43:18 -0700

    declare encoding

--- src/fobi/views/class_based.py
+++ src/fobi/views/class_based.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 """
 Class based views.
 """
@@ -492,7 +493,7 @@ class PageTitleMixin(object):
             context['page_title'] = self.get_page_title(**context)
         return context
 
-class ViewFormEntryView(FormEntryMixin, FobiThemeRedirectMixin, ProcessFormView):
+class ViewFormEntryView(FormEntryMixin,FobiThemeRedirectMixin,ProcessFormView):
     form_entry_kwarg = 'form_entry_slug'
     form_entry_query_arg = 'slug'
     form_valid_redirect = 'fobi.form_entry_submitted'
