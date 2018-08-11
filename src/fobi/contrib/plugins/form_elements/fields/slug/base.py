44b893d070f9149845fe7d70dbd17a0761c423a6
Artur Barseghyan <artur.barseghyan@gmail.com>
2018-08-11 22:43:10 +0200

    fixes

--- src/fobi/contrib/plugins/form_elements/fields/slug/base.py
+++ src/fobi/contrib/plugins/form_elements/fields/slug/base.py
@@ -41,7 +41,15 @@ class SlugInputPlugin(FormFieldPlugin):
             'required': self.data.required,
             'widget': TextInput(attrs=widget_attrs),
         }
-        if self.data.max_length is not None:
-            field_kwargs['max_length'] = int(self.data.max_length)
+
+        if self.data.max_length:
+            try:
+                field_kwargs['max_length'] = int(self.data.max_length)
+            except ValueError:
+                field_kwargs['max_length'] = None
+        else:
+            field_kwargs['max_length'] = None
+
+        field_kwargs['min_length'] = None
 
         return [(self.data.name, SlugField, field_kwargs)]
