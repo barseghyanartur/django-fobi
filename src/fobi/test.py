b5bf9ec9a2af56430bfb54509be75db72b1d29fc
Artur Barseghyan <artur.barseghyan@gmail.com>
2019-06-19 22:20:08 +0200

    wip

---
+++ src/fobi/test.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+
+import unittest
+
+from .tests.test_browser_build_dynamic_forms import *
+from .tests.test_core import *
+from .tests.test_dynamic_forms import *
+from .tests.test_sortable_dict import *
+
+__title__ = 'fobi.test'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2018 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+
+
+if __name__ == '__main__':
+    unittest.main()
