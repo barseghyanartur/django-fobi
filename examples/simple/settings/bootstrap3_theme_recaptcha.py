cbd8849a87616c8626ebbaefd78997c52080dca6
Artur Barseghyan <artur.barseghyan@gmail.com>
2020-12-09 23:48:21 +0100

    Compat with django 3.1

--- examples/simple/settings/bootstrap3_theme_recaptcha.py
+++ examples/simple/settings/bootstrap3_theme_recaptcha.py
@@ -2,15 +2,19 @@ from .base import *
 
 INSTALLED_APPS = list(INSTALLED_APPS)
 
-try:
+if not 'captcha' in INSTALLED_APPS:
     INSTALLED_APPS.append('captcha')
+
+if not 'fobi.contrib.plugins.form_elements.security.recaptcha' \
+       in INSTALLED_APPS:
     INSTALLED_APPS.append(
         'fobi.contrib.plugins.form_elements.security.recaptcha'
     )
-except Exception as err:
-    pass
 
-# RECAPTCHA_PUBLIC_KEY = ''
-# RECAPTCHA_PRIVATE_KEY = ''
+# Test keys are taken from official dedicated Google page
+# https://developers.google.com/recaptcha/docs/faq
+RECAPTCHA_PUBLIC_KEY = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
+RECAPTCHA_PRIVATE_KEY = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
 RECAPTCHA_USE_SSL = True
+SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']
 # FOBI_DEFAULT_THEME = 'simple'
