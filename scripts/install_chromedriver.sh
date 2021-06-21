d4a773a7d44aea34400c5f0f6446cf5cb63e9b65
Artur Barseghyan <artur.barseghyan@gmail.com>
2021-06-21 22:53:54 +0200

    Up reqs

--- scripts/install_chromedriver.sh
+++ scripts/install_chromedriver.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-wget -N http://chromedriver.storage.googleapis.com/2.42/chromedriver_linux64.zip
+rm chromedriver_linux64.zip
+wget -N https://chromedriver.storage.googleapis.com/91.0.4472.19/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 chmod +x chromedriver
 sudo mv -f chromedriver /usr/local/share/chromedriver
-sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
-sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver -f
+sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver -f
