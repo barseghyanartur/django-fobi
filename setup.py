47432c6dbbf2f0aedc455463b0bb064261e25e27
Andrew Sanchez <inbox.asanchez@gmail.com>
2022-03-15 13:11:10 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from distutils.version import LooseVersion
 from setuptools import setup, find_packages
 
-version = '0.17.1'
+version = '0.20.0'
 
 # ***************************************************************************
 # ************************** Django version *********************************
