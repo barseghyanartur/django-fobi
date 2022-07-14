678ca5ccd3560cda8b777573c8d2b5a480f9aa2e
gabn88 <gerben_m88@hotmail.com>
2022-07-14 15:54:45 +0200

    Added updated to db_store handler
    To keep track of updates to the model

--- src/fobi/contrib/plugins/form_handlers/db_store/models.py
+++ src/fobi/contrib/plugins/form_handlers/db_store/models.py
@@ -44,8 +44,11 @@ class AbstractSavedFormDataEntry(models.Model):
         _("Form data headers"), null=True, blank=True
     )
     saved_data = models.TextField(_("Plugin data"), null=True, blank=True)
+    
     created = models.DateTimeField(_("Date created"), auto_now_add=True)
-
+    updated = models.DateTimeField(
+        _("Updated"), null=True, blank=True, auto_now=True
+    )
     class Meta(object):
         """Meta options."""
 
