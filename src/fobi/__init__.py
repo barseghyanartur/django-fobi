be95db4d94a27b466811d02ecdaff634a3a2838d
gabn88 <gerben_m88@hotmail.com>
2023-03-20 19:29:05 +0100

    Update __init__.py

--- src/fobi/__init__.py
+++ src/fobi/__init__.py
@@ -4,4 +4,5 @@ __author__ = "Artur Barseghyan <artur.barseghyan@gmail.com>"
 __copyright__ = "2014-2022 Artur Barseghyan"
 __license__ = "GPL 2.0/LGPL 2.1"
 
-default_app_config = "fobi.apps.Config"
+if django.VERSION < (3, 2): # pragma: no cover
+    default_app_config = "fobi.apps.Config"
